20efd07d4783aa91b37361c454008f36cdfc0069
Dimitr Momperousse <dimitraye94@gmail.com>
2026-03-16 01:50:51 +0100

    STEP 3 : setup navigation compose

---
+++ app/src/main/java/com/example/mangatrack_v2/ui/navigation/Routes.kt
@@ -0,0 +1,13 @@
+package com.example.mangatrack_v2.ui.navigation
+
+object Routes  {
+
+    const val HOME = "home"
+    const val LIBRARY = "library"
+    const val MANGA_DETAILS = "manga_details"
+    const val ADD_MANGA = "add_manga"
+    const val PROFILE = "profile"
+    const val SETTINGS = "settings"
+    const val USER_STATS = "user_stats"
+
+}
