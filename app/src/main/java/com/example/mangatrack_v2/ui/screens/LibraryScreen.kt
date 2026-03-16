8402deac25ff61ec6e9c8514cdefa2cb6a3fcc00
Dimitr Momperousse <dimitraye94@gmail.com>
2026-03-16 03:27:14 +0100

    STEP 7 : implement manga details screen and navigation

--- app/src/main/java/com/example/mangatrack_v2/ui/screens/LibraryScreen.kt
+++ app/src/main/java/com/example/mangatrack_v2/ui/screens/LibraryScreen.kt
@@ -3,7 +3,6 @@ package com.example.mangatrack_v2.ui.screens
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
