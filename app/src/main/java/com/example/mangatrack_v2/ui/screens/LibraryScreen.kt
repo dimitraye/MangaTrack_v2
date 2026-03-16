7db5e60f7ba8476e95ae351cd455592d14b12dbe
Dimitr Momperousse <dimitraye94@gmail.com>
2026-03-16 03:31:25 +0100

    STEP 7 : implement manga details screen and navigation

--- app/src/main/java/com/example/mangatrack_v2/ui/screens/LibraryScreen.kt
+++ app/src/main/java/com/example/mangatrack_v2/ui/screens/LibraryScreen.kt
@@ -1,8 +1,12 @@
 package com.example.mangatrack_v2.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
@@ -13,26 +17,48 @@ import com.example.mangatrack_v2.ui.navigation.Routes
 import com.example.mangatrack_v2.viewmodel.MangaViewModel
 
 @Composable
-fun LibraryScreen(navController: NavController, viewModel: MangaViewModel = viewModel()) {
+fun LibraryScreen(
+    navController: NavController,
+    viewModel: MangaViewModel = viewModel()
+) {
 
     val mangas = viewModel.mangas.collectAsState(initial = emptyList())
 
-    LazyColumn(
-        modifier = Modifier.fillMaxSize()
-    ) {
+    Scaffold(
 
-        items(mangas.value) { manga ->
-
-            MangaCard(
-                manga = manga,
+        floatingActionButton = {
+            FloatingActionButton(
                 onClick = {
-                    navController.navigate(
-                        Routes.mangaDetails(manga.id)
-                    )
+                    navController.navigate(Routes.ADD_MANGA)
                 }
-            )
+            ) {
+                Text("+")
+            }
+        }
+
+    ) { paddingValues ->
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+
+            items(mangas.value) { manga ->
+
+                MangaCard(
+                    manga = manga,
+                    onClick = {
+                        navController.navigate(
+                            Routes.mangaDetails(manga.id)
+                        )
+                    }
+                )
+
+            }
 
         }
 
     }
+
 }
